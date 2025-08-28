01e886a6cd2e9298a9767be93114dd3562a9a951
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-08-28 18:33:54 +0200

    Lesson 2 - Prepared

---
+++ Lessons/2/ex1.py
@@ -0,0 +1,3 @@
+"""
+Stwórz kilka zmiennych, przypisz im różne wartości i wypisz je na ekranie.
+"""
---
+++ Lessons/2/ex2.py
@@ -0,0 +1,9 @@
+"""
+Stwórz 4 zmienne w których zapiszesz swoje:
+- imię,
+- wzrost (w metrach),
+- wiek (wlatach)
+- i to czy jesteś pełnoletni.
+
+Następnie wypisz je na konsoli. Zmienne powinny być odpowiednio ponazywane.
+"""
