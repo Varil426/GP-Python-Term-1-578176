83dd954d241e18077709b752d2aab7764cef369f
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-10-18 11:38:52 +0200

    Lesson 9 - Completed

--- Lessons/9/ex2.py
+++ Lessons/9/ex2.py
@@ -4,3 +4,14 @@ Przygotuj 3 listy, a następnie wyświetl się w konsoli, aby sprawdzić czy zos
 - Listę minimum 3 ulubionych gier / filmów / piosenek.
 - Listę składającą się z 3 list, w których odpowiednio wpiszesz Imię, Nazwisko i Wiek trzech wymyślonych osób.
 """
+
+# a = [b,c,d]
+# print(lista)
+
+lista_losowych_liczb = [1, 3, 8, 9, 7.2]
+
+lista_ulubionych_gier_filmow = ["Skate", "GTA SA", "Alan Wake"]
+
+osoby = [["Bartek", "Kręgielewski", 26], ["Kuba", "Test", 11], ["Tomasz", "Bagiński", 49]]
+
+print(lista_losowych_liczb, lista_ulubionych_gier_filmow, osoby)
--- Lessons/9/ex3.py
+++ Lessons/9/ex3.py
@@ -4,3 +4,27 @@ Na koniec wyświetli je w tej samej kolejności.
 
 Dodatkowo: Spróbuj wyświetlić komunikaty w odwrotnej kolejności.
 """
+
+# 1. Program pyta o liczbę elementów ✅
+# 2. Program odczytuje podaną ilość komunikatów ✅
+# 3. Program wyświetla je w tej samej kolejnośći ✅
+# 4. DODATKOWE: Wyświetlenie w odwrotnej kolejności ✅
+
+ilosc_komunikatow = int(input("Podaj ilość komunikatów: "))
+tabela_komunikatow = []
+
+for _ in range(ilosc_komunikatow):
+    komunikat = input("Podaj komunikat: ")
+    tabela_komunikatow.append(komunikat)
+
+# Wyświetlamy je testowo w jednej linijce
+print(tabela_komunikatow)
+
+# Wyświetlanie w kolejności pierwotnej
+for element in tabela_komunikatow:
+    print(element)
+
+# Wyświetlanie w kolejności odwrotnej
+for przesuniecie in range(len(tabela_komunikatow)):
+    index = len(tabela_komunikatow) - 1 - przesuniecie
+    print(tabela_komunikatow[index])
