467958070aca49ca27fced52b31af764127c290c
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2025-12-25 13:07:45 +0100

    Lesson 18 - Prepared

---
+++ Lessons/18/yahtzee.py
@@ -0,0 +1,21 @@
+import random
+
+kosci = [2, 1, 3, 6, 4]
+
+nazwy_punktow = [
+    "Jedynki",
+    "Dwójki",
+    "Trójki",
+    "Czwórki",
+    "Piątki",
+    "Szóstki",
+    "3 jednakowe",
+    "4 jednakowe",
+    "Full",
+    "Mały strit",
+    "Duży strit",
+    "Generał",
+    "Szansa",
+]
+
+punkty = ["", "", "", "", "", "", "", "", "", "", "", "", ""]
