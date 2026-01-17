0173494878924dbfaea9cb5e6abf5d7ba687b936
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2026-01-17 11:43:22 +0100

    Lesson 21 - Completed

--- Lessons/21/ex1.py
+++ Lessons/21/ex1.py
@@ -1,6 +1,41 @@
 """
 Stwórz klasy: `Kolo` i `Kwadrat`.
 Klasy powinny przechowywać informację o figurze (promień lub bok).
-Dodaj metody do obliczania pola i obwodu.
+Dodaj metody do obliczania pola i obwodu - powinny zwracać wartości.
 Pamiętaj, aby stworzyć odpowiedni konstruktor.
 """
+
+# Koło
+# r - promień; Pi - stała Pi, 3.14, math.pi
+# Pole: Pi*r^2
+# Obwód: 2*Pi*r
+
+# Kwadrat
+# a - długość boku kwadratu
+# Pole: a*a
+# Obwód: 4*a
+
+# Konstruktor `__init__`
+
+from math import pi
+
+class Kolo:
+    def __init__(self, promien):
+        self.promien = promien
+
+    def oblicz_pole(self):
+        pole = pi * self.promien ** 2
+        return pole
+    
+    def oblicz_obwod(self):
+        return 2 * pi * self.promien
+    
+# Test - Koło
+k1 = Kolo(10)
+k2 = Kolo(20)
+
+print(k1.oblicz_obwod())
+print(k1.oblicz_pole())
+
+print(k2.oblicz_obwod())
+print(k2.oblicz_pole())
--- Lessons/21/main.py
+++ Lessons/21/main.py
@@ -1,10 +1,33 @@
 class Samochod:
-    marka = ""
-    model = ""
-    typSilnika = ""
-    mocKM = 0
+    # marka = ""
+    # model = ""
+    # typSilnika = ""
+    # mocKM = 0
+    licznik = 0
 
     def wyswietl(self):
         print(self.marka)
         print(self.model)
         print(self.typSilnika, self.mocKM)
+        print(f"Wszystkich samochodów jest: {Samochod.licznik}")
+
+    def __init__(self, marka, model, typSilnika, mocKM):
+        print("Tworzenie obiektu Samochod")
+        self.marka = marka
+        self.model = model
+        self.typSilnika = typSilnika
+        self.mocKM = mocKM
+        Samochod.licznik += 1
+
+s1 = Samochod("Pagani", "Zonda 760 Venti", "Benza", 760)
+# s1.marka = "Pagani"
+# s1.model = "Zonda 760 Venti"
+# s1.typSilnika = "Benza"
+# s1.mocKM = 760
+s1.wyswietl()
+
+s2 = Samochod("BMW", "E36", "Benza", 102)
+s2.wyswietl()
+
+s3 = Samochod("Ferrari", "Bonzo", "Benza", 660)
+s3.wyswietl()
