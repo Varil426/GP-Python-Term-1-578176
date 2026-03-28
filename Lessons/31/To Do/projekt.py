f8a992f7a986d939e3e10ed7b47d2e9f4591dc79
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2026-03-28 11:38:28 +0100

    Lesson 31 - Completed

--- Lessons/31/To Do/projekt.py
+++ Lessons/31/To Do/projekt.py
@@ -3,6 +3,7 @@ import pygame
 from constants import SZEROKOSC_EKRANU, WYSOKOSC_EKRANU
 from platforma import Platforma
 from kulka import Kulka
+from klocek import Klocek
 
 # Ustawienie katalogu roboczego
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -14,9 +15,69 @@ zegar = pygame.time.Clock()
 obraz_tla = pygame.image.load("images/background.png")
 czcionka = pygame.font.SysFont("Comic Sans MS", 24)
 
+# Poziomy gry
+poziom0 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+]
+
+poziom1 = [
+	[0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+	[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+	[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+	[0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+poziom2 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+]
+
+poziom3 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+]
+
 platforma = Platforma()
 kulka = Kulka()
 Zycia = 3
+Poziom = 0
+klocki = pygame.sprite.Group()
+
+def dodaj_klocki():
+    wczytany_poziom = None
+    if Poziom == 0:
+        wczytany_poziom = poziom1
+    if Poziom == 1:
+        wczytany_poziom = poziom2
+    if Poziom == 2:
+        wczytany_poziom = poziom3
+    
+    for i in range(10):
+        for j in range(7):
+            if wczytany_poziom[j][i] != 0:
+                klocek = Klocek(32+i*96, 32+j*48, wczytany_poziom[j][i])
+                klocki.add(klocek)
+
+dodaj_klocki()
 
 gra_dziala = True
 while gra_dziala:
@@ -33,8 +94,17 @@ while gra_dziala:
     if keys[pygame.K_d]:
         platforma.ruszaj_platforma(1)
 
+    if len(klocki.sprites()) == 0:
+        Poziom += 1
+        if Poziom >= 3:
+            break
+        kulka.zresetuj_pozycje()
+        platforma.zresetuj_pozycje()
+        dodaj_klocki()
+
+    klocki.update()
     platforma.aktualizuj()
-    kulka.aktualizuj(platforma)
+    kulka.aktualizuj(platforma, klocki)
     if kulka.przegrana:
         Zycia -= 1
         if Zycia <= 0:
@@ -45,6 +115,8 @@ while gra_dziala:
     ekran.blit(obraz_tla, (0,0))
     ekran.blit(platforma.obraz, platforma.rect)
     ekran.blit(kulka.obraz, kulka.rect)
+    for klocek in klocki:
+        ekran.blit(klocek.obraz, klocek.rect)
 
     tekst = czcionka.render(f"Życia: {Zycia}", False, (255, 0, 255))
     ekran.blit(tekst, (16,16))
